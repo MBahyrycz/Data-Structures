1babed91c1615c0769ae6c7c276310028ba2a19c
MBahyrycz <m.bahyrycz@gmail.com>
2021-01-31 17:56:19 +0100

    Vector added TODO: ITERATORS

--- DataStructures/Examples.cpp
+++ DataStructures/Examples.cpp
@@ -1,16 +1,21 @@
 #include "DataStructures.h"
 #include <iostream>
 
-#include <array>
-
-#define _CONTAINER_DEBUG 1
+/* * * *
+  * comment sellected #define ... to switch off corresponding example
+  * * * *
+  */
+//#define _ARRAY_EXAMPLE
+#define _VECTOR_EXAMPLE
 
 int main()
 {
-	Array<int, 3> array;
-	array[0] = 0;
+#ifdef _ARRAY_EXAMPLE 
+	Array<int, 5> array;
+
+	memset(array.Data(), 0, array.Size() * sizeof(int));
 	array[1] = 7;
-	array[2] = 3;
+	array[4] = 3;
 
 	for (size_t i = 0; i < array.Size(); i++)
 	{
@@ -19,6 +24,49 @@ int main()
 
 	int* ptr = array.Data();
 	std::cout << *(ptr + 1);
+#endif // _ARRAY_EXAMPLE
+
+#ifdef _VECTOR_EXAMPLE
+	Vector<float> vector;
+	vector.PushBack(1.9f);
+	vector.PushBack(1.72f);
+	vector.PushBack(2.0f);
+	vector.EmplaceBack(2.3f);
+	vector.EmplaceBack(4.45f);
+	vector.EmplaceBack(5.56f);
+	vector.PushBack(7.0f);
+	vector.PushBack(2.34f);
+	vector[5] = 0.0f;
+	vector[6] = 0.0f;
+
+	for (size_t i = 0; i < vector.Size(); i++)
+	{
+		std::cout << vector[i] << std::endl;
+	}
+	std::cout << "===============================" << std::endl;
+
+	vector.PopBack();
+	vector.PopBack();
+
+	for (size_t i = 0; i < vector.Size(); i++)
+	{
+		std::cout << vector[i] << std::endl;
+	}
+	std::cout << "===============================" << std::endl;
+
+
+	vector.Clear();
+
+	for (size_t i = 0; i < vector.Size(); i++)
+	{
+		std::cout << vector[i] << std::endl;
+	}
+	std::cout << "===============================" << std::endl;
+
+
+#endif // _VECTOR_EXAMPLE
+
+	std::cin.get();
 
 	return 0;
 }
--- DataStructures/src/DataStructures.h
+++ DataStructures/src/DataStructures.h
@@ -4,22 +4,23 @@
 /* * * * 
  *  One header file library with simple implementation of
  *  basic STL data structures. Easy to use, easy to modify,
- *  easy to add new functionalities.
+ *  easy to add new features.
  * * * *
  */
 
 #include <memory>
 
  /* * * *
-  * define "_CONTAINER_DEBUG" to 0 to switch off debug mode
+  * change value of _CONTAINER_DEBUG to 0 to switch off debug mode
   * * * *
   */
 #define _CONTAINER_DEBUG 1
   
 
  /* * * *
-  *  Array class container - simple stack alocated array.
-  *  Can bespecified as array of each type.
+  *  Array class container - simple stack alocated array, that
+  *  stores data continuously.
+  *  Can be specified as array of each type.
   *  
   * * * *
   */
@@ -31,13 +32,11 @@ public:
     constexpr size_t Size() { return S; }
 
     T* Data() { return m_Data; }
-
     const T* Data() const { return m_Data; }
 
     T& operator[](size_t index)
     {
-
-#ifdef _CONTAINER_DEBUG > 0
+#if _CONTAINER_DEBUG > 0
         if (!(index < S))
             __debugbreak(); // INDEX OUT OF RANGE
 #endif // _CONTAINER_DEBUG
@@ -46,18 +45,140 @@ public:
 
     const T& operator[](size_t index) const
     {
-#ifdef _CONTAINER_DEBUG > 0
+#if _CONTAINER_DEBUG > 0
         if (!(index < S))
             __debugbreak(); // INDEX OUT OF RANGE
 #endif // _CONTAINER_DEBUG
         return m_Data[index];
     }
 
-    //TODO: iterators
+    //TODO: iterators, empty(), fill()
 private:
     T m_Data[S];
 };
 
+/* * * *
+ *  Vector class container - simple dynamically alocated container, that
+ *  stores data continuously. All data is stored on heap.
+ *  Can be specified as array of each type.
+ *
+ * * * *
+ */
+
+template<typename T>
+class Vector
+{
+public:
+    Vector()
+    {
+        // alocate size for 2 elements at the start, can be changed if needed
+        ReAlloc(2);
+    }
+
+    ~Vector()
+    {
+        Clear();
+        ::operator delete(m_Data, m_Capacity * sizeof(T));
+    }
+
+    void PushBack(const T& value)
+    {
+        if (m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);   // growing vector size by 50%, can be changed if needed
+
+        m_Data[m_Size] = value;
+        m_Size++;
+    }
+
+    void PushBack(T&& value)
+    {
+        if (m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);   // growing vector size by 50%, can be changed if needed
+
+        m_Data[m_Size] = std::move(value);
+        m_Size++;
+    }
+
+    template<typename... Args>
+    T& EmplaceBack(Args&&... args)
+    {
+        if (m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);   // growing vector size by 50%, can be changed if needed
+
+        new(&m_Data[m_Size]) T(std::forward<Args>(args)...);
+  
+        return m_Data[m_Size++];
+    }
+
+    void PopBack()
+    {
+        if (m_Size > 0)
+        {
+            m_Size--;
+            m_Data[m_Size].~T();
+        }
+    }
+
+    void Clear()
+    {
+        for (size_t i = 0; i < m_Size; i++)
+            m_Data[i].~T();
+
+        m_Size = 0;
+    }
+
+    T& operator[](size_t index)
+    {
+#if _CONTAINER_DEBUG > 0
+        if (!(index < m_Size))
+            __debugbreak(); // INDEX OUT OF RANGE
+#endif // _CONTAINER_DEBUG
+        return m_Data[index];
+    }
+
+    const T& operator[](size_t index) const
+    {
+#if _CONTAINER_DEBUG > 0
+        if (!(index < m_Size))
+            __debugbreak(); // INDEX OUT OF RANGE
+#endif // _CONTAINER_DEBUG
+        return m_Data[index];
+    }
+
+    constexpr size_t Size() const { return m_Size; }
+private:
+    void ReAlloc(size_t newCapacity)
+    {
+        // 1. allocate a new block of memory
+        // 2. moving stored elements to new block
+        // 3. freeing memory
+
+
+        /* BUG WITH ALLOCATING MEMORY WITHOUT CONTRUCTORS IN STD::STRING */
+        T* newBlock = (T*)::operator new(newCapacity * sizeof(T));
+        //T* newBlock = (T*)malloc(newCapacity * sizeof(T));
+        //T* newBlock = new T[newCapacity];
+
+        if (newCapacity < m_Size)
+            m_Size = newCapacity;                   // for realocating smaller vector than before
+
+        for (size_t i = 0; i < m_Size; i++)
+            newBlock[i] = std::move(m_Data[i]);     // memcpy can cause some problems with higher lvl structures as classes
+        
+        for (size_t i = 0; i < m_Size; i++)
+            m_Data[i].~T();
+
+        ::operator delete(m_Data, m_Capacity * sizeof(T));
+        //delete[] m_Data;
+        m_Data = newBlock;
+        m_Capacity = newCapacity;
+    }
+private:
+    T* m_Data = nullptr;
+
+    size_t m_Size = 0;
+    size_t m_Capacity = 0;
+};
 
 
 #endif
